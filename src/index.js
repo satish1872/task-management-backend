2b84c3f1c7b23bc637cad24315de6d07c53bcd78
satish1872 <41827034+satish1872@users.noreply.github.com>
2024-03-14 17:14:15 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const authRoutes = require("./routes/authRoutes");
 const taskRoutes = require("./routes/taskRoutes");
 const cors = require("cors");
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
